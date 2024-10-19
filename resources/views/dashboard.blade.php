8c1fc71eeab5551aa25b9a785977d5334b307e9f
M-H-Riad <meshkathasanriad@gmail.com>
2024-10-19 12:22:20 +0600

    Initial project setup

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.admin')
+
+@section('content')
+<style>
+  .dashboard {
+    height: 700px;
+  }
+</style>
+<section class="section dashboard">
+    <div class="row">
+
+      <!-- Left side columns -->
+      <div class="col-lg-12">
+        <div class="row">
+
+          <!-- Event Card -->
+          <div class="col-xxl-3 col-md-6">
+            <div class="card info-card revenue-card">
+
+              <div class="card-body">
+                <h5 class="card-title">Total Events</h5>
+
+                <div class="d-flex align-items-center">
+                  <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                    <i class="bi bi-card-list"></i>
+                  </div>
+                  <div class="ps-3">
+                    <h6>64</h6>
+
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div><!-- End Event Card -->
+
+          
+
+    </div>
+  </section>
+@endsection
